d190d6cfcd18da27882b96bec9a992718583322a
Chase <chasepoblete@gmail.com>
2026-08-25 10:34:20 -0700

    Manually Enter People

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 const form = document.querySelector("form");
 const resultText = document.querySelector("#result");
 
+const manualOption = document.querySelector("#form__manual");
+const peopleEnter = document.querySelector("#form__people");
+const occupancyOptions = document.querySelectorAll(
+  'input[name="occupancy-use"]',
+);
+
+function updateManualPeopleState() {
+  const selectedOccupancy = document.querySelector(
+    'input[name="occupancy-use"]:checked',
+  );
+
+  const length = document.querySelector("#length");
+  const width = document.querySelector("#width");
+
+  const manualSelected = selectedOccupancy === manualOption;
+  peopleEnter.disabled = !manualSelected;
+  length.disabled = manualSelected;
+  width.disabled = manualSelected;
+
+  length.placeholder = manualSelected ? "Not required." : "";
+  width.placeholder = manualSelected ? "Not required." : "";
+
+  peopleEnter.required = manualSelected;
+
+  if (!manualSelected) {
+    peopleEnter.value = "";
+  }
+}
+
+occupancyOptions.forEach((option) => {
+  option.addEventListener("change", updateManualPeopleState);
+});
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
@@ -23,16 +56,23 @@ form.addEventListener("submit", (event) => {
 
   const lengthAmt = Number(length.value);
   const widthAmt = Number(width.value);
-  const occupancyFactor = Number(selectedOccupancy.value);
   const exitFootageAmt = Number(exitFootage.value);
   const exitDischargeFactor = Number(selectedExitDischarge.value);
 
-  let box1 = lengthAmt * widthAmt;
-  box1 /= occupancyFactor;
+  let occupantLoad;
+
+  if (selectedOccupancy === manualOption) {
+    occupantLoad = Number(peopleEnter.value);
+  } else {
+    const occupancyFactor = Number(selectedOccupancy.value);
+    occupantLoad = (lengthAmt * widthAmt) / occupancyFactor;
+  }
+
+  console.log(occupantLoad);
 
   let box2 = exitFootageAmt * exitDischargeFactor;
 
-  let result = (box1 / box2) * 100;
+  let result = (occupantLoad / box2) * 100;
 
   resultText.textContent = `Occupancy readiness: ${result.toFixed(2)}%`;
 });
